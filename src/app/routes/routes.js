eb42707892f68fda980fcc84a90400a0b1aebc43
Benicio Daniel Barreto <beni@MacBook-Pro-de-Benicio.local>
2020-02-09 23:24:09 -0300

    Install Marko

--- src/app/routes/routes.js
+++ src/app/routes/routes.js
@@ -15,17 +15,8 @@ module.exports = (app) => {
   });
   
   app.get('/livros', (req, res) => {
-    res.send(
-        `
-        <html>
-            <head>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1> Books List </h1>
-            </body> 
-        </html>
-        `
+    res.marko(
+      require('../views/books/list/list.marko')
     )
   });
 };
