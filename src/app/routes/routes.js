0456d588703d222931761bcc7ad8e764e9c8fbbd
Benicio Daniel Barreto <beni@MacBook-Pro-de-Benicio.local>
2020-02-23 15:56:47 -0300

    Refactoring the routes in different files

--- src/app/routes/routes.js
+++ src/app/routes/routes.js
@@ -1,30 +1,8 @@
-
-const HomeController = require('../controllers/HomeController');
-const homeController = new HomeController();
-
-const BookController = require('../controllers/BookController');
-const bookController = new BookController();
-
-const Book = require('../models/Books');
+const bookRoutes = require('./book-routes');
+const baseRoutes = require('./base-routes');
 
 module.exports = (app) => {
-  const homeRoutes = HomeController.routes();
-  const booksRoutes = BookController.routes();
-
-  app.get(homeRoutes.home, homeController.home());
-  
-  app.get(booksRoutes.list, bookController.list());
-  
-  app.get(booksRoutes.register, bookController.formCreate());
-  
-  app.get(booksRoutes.edit, bookController.formEdit());
-
-  app.post(booksRoutes.register, Book.validations(), bookController.register());
-
-  app.put(booksRoutes.register, bookController.edit());
-
-  app.delete(booksRoutes.remove, bookController.remove());
-
-  app.get(booksRoutes.detail, bookController.detail());
+  baseRoutes(app);
+  bookRoutes(app);
 }
 
